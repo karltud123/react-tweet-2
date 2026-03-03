b71c58535db1c4f7a43cfb31506d9355bb24c6fa
Karl Michael Gutierrez Linantud <kmlinantud@gmail.com>
2026-03-03 10:53:06 +0800

    chore: create custom declaration file

--- packages/react-tweet/global.d.ts
+++ packages/react-tweet/global.d.ts
@@ -1,11 +1,4 @@
-import 'react'
 declare module '*.module.css' {
   const classes: { readonly [key: string]: string }
   export default classes
-}
-
-declare module 'react' {
-  interface VideoHTMLAttributes<HTMLVideoElement> {
-    referrerPolicy?: string
-  }
 }
