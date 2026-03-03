cb77978b93a6fcd2ceb3febd73e3e8ce59ceca94
Karl Michael Gutierrez Linantud <kmlinantud@gmail.com>
2026-03-03 10:48:53 +0800

    chore: cleanup

--- packages/react-tweet/global.d.ts
+++ packages/react-tweet/global.d.ts
@@ -1,4 +1,11 @@
+import 'react'
 declare module '*.module.css' {
   const classes: { readonly [key: string]: string }
   export default classes
+}
+
+declare module 'react' {
+  interface VideoHTMLAttributes<HTMLVideoElement> {
+    referrerPolicy?: string
+  }
 }
--- packages/react-tweet/src/custom.d.ts
+++
@@ -1,8 +0,0 @@
-// custom.d.ts or similar declaration file
-import 'react'
-
-declare module 'react' {
-  interface VideoHTMLAttributes<HTMLVideoElement> {
-    referrerPolicy?: string
-  }
-}
